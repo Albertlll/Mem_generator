cacde6a71827eff6b74313891df4f6285b2a8f7d
Albertlll <90716405+Albertlll@users.noreply.github.com>
2021-10-28 05:40:18 +0300

    Add files via upload

--- mem_gen_tests.py
+++ mem_gen_tests.py
@@ -1,11 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QListWidgetItem, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QListWidgetItem, QWidget, QColorDialog,\
+    QListWidget, QPushButton, QSpinBox
 from PyQt5 import uic
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QColor
 from PIL import Image, ImageDraw
-
+# QSpinBox.changeEvent()
 IMAGE_ON_APP_SIZES = (500, 500)
-
 
 class TextOnMem:
     def __init__(self):
@@ -18,23 +18,14 @@ class TextOnMem:
     def setx(self, x):
         self.x = x
 
-    def getx(self):
-        return self.x
-
     def sety(self, y):
         self.y = y
-
-    def gety(self):
-        return self.y
 
     def settext(self, text):
         self.text = text
 
     def setfont(self, font):
         self.font = font
-
-    def getcolor(self):
-        return self.color
 
     def __str__(self):
         return self.text
@@ -56,8 +47,8 @@ class MyPicture(QWidget):
         self.reload_texts()
 
     def load_image(self):
-        fname = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '')[0]
-        self.pil_file = Image.open(fname)
+        self.fname = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '')[0]
+        self.pil_file = Image.open(self.fname)
         self.pil_file.thumbnail(IMAGE_ON_APP_SIZES)
 
         self.reload_texts()
@@ -65,6 +56,8 @@ class MyPicture(QWidget):
         self.load_btn.setEnabled(False)
 
     def reload_texts(self):
+        self.pil_file = Image.open(self.fname)
+        self.pil_file.thumbnail(IMAGE_ON_APP_SIZES)
         draw_pil_file = ImageDraw.Draw(self.pil_file)
         for i in range(len(self.texts)):
             txt = self.texts[i]
@@ -76,15 +69,41 @@ class MyPicture(QWidget):
         self.image.setPixmap(self.pixmap)
 
     def open_parametrs(self):
-        self.parametrs = TextParametrs()
+        print(self.sender().currentRow())
+        self.parametrs = TextParametrs(self.sender().currentRow(), self.texts, self)
         self.parametrs.show()
         self.setEnabled(True)
 
 
 class TextParametrs(QWidget):
-    def __init__(self):
+    def __init__(self, sender_txt, texts, self_of_form):
         super().__init__()
+        self.self_of_form = self_of_form
+        self.sndr = texts[sender_txt]
+        print(type(self.sndr))
+        self.color = self.sndr.color
+        self.x = self.sndr.x
+        self.y = self.sndr.y
+
         uic.loadUi('text_parametrs_ui.ui', self)
+        self.pushButton_3.clicked.connect(self.choise_color)
+        self.spinBox_4.valueChanged.connect(self.save)
+        self.spinBox_3.valueChanged.connect(self.save)
+
+    def choise_color(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.color = color.getRgb()
+            self.pushButton_3.setStyleSheet(
+                f"background-color: rgb("
+                f"{', '.join(list(str(i) for i in self.color))});")
+
+    def save(self):
+        print("fff")
+        self.sndr.color = self.color
+        self.sndr.x = self.spinBox_4.value()
+        self.sndr.y = self.spinBox_3.value()
+        MyPicture.reload_texts(self.self_of_form)
 
 
 def except_hook(cls, exception, traceback):
@@ -93,7 +112,7 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    form = MainForm()
+    form = MyPicture()
     form.show()
     sys.excepthook = except_hook
     sys.exit(app.exec())
